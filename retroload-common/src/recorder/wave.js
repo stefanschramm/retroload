15b4e9cdfa0d721c3a6b996235c73fb9d91235fd
Stefan Schramm <mail@stefanschramm.net>
2023-01-20 09:20:59 +0100

    Speed up and simpify creation of WAVE data

--- retroload-common/src/recorder/wave.js
+++ retroload-common/src/recorder/wave.js
@@ -12,15 +12,17 @@ export class WaveRecorder {
     this.sampleRate = sampleRate;
   }
   pushSample(value) {
-    this.data.push(value);
+    this.data.push(dataMap[value]);
   }
   getBuffer() {
     const format = 1; // pcm
     const channels = 1;
     const bitsPerSample = 8;
     const formatChunkSize = 16;
-    const dataChunkSize = this.data.length * channels * Math.floor(bitsPerSample / 8);
-    const chunkSize = 4 + 8 + formatChunkSize + 8 + dataChunkSize;
+
+    const headerSize = 4 + 8 + formatChunkSize + 8;
+    const chunkSize = headerSize + this.data.length;
+
     const byteRate = this.sampleRate * channels * Math.floor(bitsPerSample / 8);
     const blockAlign = channels * Math.floor(bitsPerSample / 8);
 
@@ -31,6 +33,7 @@ export class WaveRecorder {
     bufferWriter.write('RIFF');
     bufferWriter.writeUInt32LE(chunkSize);
     bufferWriter.write('WAVE');
+
     // Format section
     bufferWriter.write('fmt ');
     bufferWriter.writeUInt32LE(formatChunkSize);
@@ -40,25 +43,17 @@ export class WaveRecorder {
     bufferWriter.writeUInt32LE(byteRate);
     bufferWriter.writeUInt16LE(blockAlign);
     bufferWriter.writeUInt16LE(bitsPerSample);
+
     // Data section
     bufferWriter.write('data');
-    bufferWriter.writeUInt32LE(dataChunkSize);
+    bufferWriter.writeUInt32LE(this.data.length);
 
-    for (const sample of this.data) {
-      bufferWriter.writeUInt8(dataMap[sample]);
-    }
+    const uint8Array = new Uint8Array(buffer);
+    uint8Array.set(this.getRawBuffer(), bufferWriter.offset);
 
-    return new Uint8Array(buffer);
+    return uint8Array;
   }
   getRawBuffer() {
-    const channels = 1;
-    const bitsPerSample = 8;
-    const dataChunkSize = this.data.length * channels * Math.floor(bitsPerSample / 8);
-    const buffer = new ArrayBuffer(dataChunkSize);
-    const bufferWriter = new ArrayBufferWriter(buffer);
-    for (const sample of this.data) {
-      bufferWriter.writeUInt8(dataMap[sample]);
-    }
-    return new Uint8Array(buffer);
+    return new Uint8Array(this.data);
   }
 }
