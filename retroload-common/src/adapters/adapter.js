dbc20682fa55a6feaeec9f01a13e475063e38d5d
Stefan Schramm <mail@stefanschramm.net>
2023-02-23 22:42:25 +0100

    Remove dummy method

--- retroload-common/src/adapters/adapter.js
+++ retroload-common/src/adapters/adapter.js
@@ -1,9 +1,4 @@
-import {InternalError} from '../exception.js';
-
 export class AbstractAdapter {
-  static getTargetName() {
-    throw new InternalError('getTargetName() not implemented!');
-  }
   static getOptions() {
     return [];
   }
