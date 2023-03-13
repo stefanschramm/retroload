349c0dbd6fa23e824a5f0f523b34ad73dbd4c96f
Stefan Schramm <mail@stefanschramm.net>
2023-03-13 22:06:37 +0100

    Expose BufferAccess

--- retroload-common/src/index.js
+++ retroload-common/src/index.js
@@ -1,3 +1,4 @@
+export {BufferAccess} from './BufferAccess.js';
 export {PcmRecorder as PcmRecorder} from './recorder/PcmRecorder.js';
 export {WaveRecorder as WaveRecorder} from './recorder/WaveRecorder.js';
 export * as Exception from './Exceptions.js';
