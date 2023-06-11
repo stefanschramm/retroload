c34e2df249cfd0a3cc0470879696a1f72bc529ef
Stefan Schramm <mail@stefanschramm.net>
2023-06-11 17:01:23 +0200

    Migrate CLI to TS

--- retroload-cli/src/tokenizer.ts
+++ retroload-cli/src/tokenizer.ts
@@ -6,26 +6,25 @@ import {BasicTokenizers} from 'retroload-basic';
 
 function main() {
   const program = (new Command())
-      .name('retroload-tokenizer')
-      .description('Convert plain text BASIC files into tokenized BASIC files for loading.')
-      .argument('<infile>', 'Path to source file to tokenize')
-      .option('-d, --dialect <basic dialect>', 'BASIC dialect to use')
-      .option('-o, --outfile <output file>', 'Where to write to binary output to. By default the input filename with a dialect specific extension is used as output filename.')
-  ;
+    .name('retroload-tokenizer')
+    .description('Convert plain text BASIC files into tokenized BASIC files for loading.')
+    .argument('<infile>', 'Path to source file to tokenize')
+    .option('-d, --dialect <basic dialect>', 'BASIC dialect to use')
+    .option('-o, --outfile <output file>', 'Where to write to binary output to. By default the input filename with a dialect specific extension is used as output filename.');
   program.parse();
   const options = program.opts();
   const infile = program.args[0];
-  const dialect = options.dialect || 'kc';
+  const dialect = typeof options['dialect'] === 'string' ? options['dialect'] : 'kc';
 
   const tokenizer = getTokenizerByDialectName(dialect);
 
-  const outfile = options.outfile || `${infile}.${tokenizer.getExtension()}`;
+  const outfile = typeof options['outfile'] === 'string' ? options['outfile'] : `${infile}.${tokenizer.getExtension()}`;
 
   const destinationBa = tokenizer.tokenize(fs.readFileSync(infile).toString());
   fs.writeFileSync(outfile, destinationBa.asUint8Array());
 }
 
-function getTokenizerByDialectName(name) {
+function getTokenizerByDialectName(name: string) {
   const result = BasicTokenizers.filter((t) => t.getName() === name);
 
   if (result.length === 0) {
