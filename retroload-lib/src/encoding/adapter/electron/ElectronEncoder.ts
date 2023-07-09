744a21c1713c78b9d5f323d1271ed57ebf207f1f
Stefan Schramm <mail@stefanschramm.net>
2023-07-09 10:54:32 +0200

    Add further UEF chunk types
    Add UEF "target machine" chunk type
    
    Add UEF "floating point gap" chunk type

--- retroload-lib/src/encoding/adapter/electron/ElectronEncoder.ts
+++ retroload-lib/src/encoding/adapter/electron/ElectronEncoder.ts
@@ -1,6 +1,6 @@
 import {AbstractEncoder} from '../AbstractEncoder.js';
 
-const fBase = 1200;
+// const fBase = 1200;
 
 /**
  * Encoder for Acorn Electron
@@ -8,8 +8,10 @@ const fBase = 1200;
  * https://beebwiki.mdfs.net/Acorn_cassette_format
  */
 export class ElectronEncoder extends AbstractEncoder {
+  private fBase = 1200;
+
   recordPilot(length: number) {
-    this.recordCarrier(fBase * 2 * length);
+    this.recordCarrier(this.fBase * 2 * length);
   }
 
   override end() {
@@ -18,25 +20,49 @@ export class ElectronEncoder extends AbstractEncoder {
 
   recordCarrier(oscillations: number) {
     // UEF Chunk &0111 - carrier tone
-    this.recordOscillations(fBase * 2, oscillations);
+    this.recordOscillations(this.fBase * 2, oscillations);
   }
 
   recordGap(length: number) {
     // UEF Chunk &0112 - integer gap
-    this.recordSilenceMs(1000 / (2 * length * fBase));
+    this.recordSilenceMs(1000 / (2 * length * this.fBase));
   }
 
-  override recordByte(byte: number) {
+  override recordByte(byte: number, dataBits = 8, parity: ParitySetting = 'N', stopBits = 1) {
     this.recordBit(0); // start bit
-    super.recordByte(byte);
-    this.recordBit(1); // stop bit
+
+    // LSB first
+    let sum = 0;
+    for (let i = 0; i < dataBits; i += 1) {
+      const bit = ((byte & (1 << i)) === 0) ? 0 : 1;
+      sum += bit;
+      this.recordBit(bit);
+    }
+
+    // TODO: not sure, if parity, stopBits and dataBits is working correctly
+    if (parity === 'E') {
+      this.recordBit((sum % 2 === 0) ? 1 : 0);
+    } else if (parity === 'O') {
+      this.recordBit((sum % 2 === 0) ? 0 : 1);
+    }
+
+    // stop bits
+    for (let i = 0; i < stopBits; i++) {
+      this.recordBit(1);
+    }
   }
 
   recordBit(value: number) {
     if (value) {
-      this.recordOscillations(fBase * 2, 2);
+      this.recordOscillations(this.fBase * 2, 2);
     } else {
-      this.recordOscillations(fBase, 1);
+      this.recordOscillations(this.fBase, 1);
     }
   }
+
+  setBaseFrequency(f: number) {
+    this.fBase = f;
+  }
 }
+
+export type ParitySetting = 'N' | 'E' | 'O';
