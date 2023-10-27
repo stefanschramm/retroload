b417d624ecf7224f0c2921cb1f5593c6a2742a0b
Stefan Schramm <mail@stefanschramm.net>
2023-10-27 10:35:10 +0200

    Add tests for hex* functions

--- retroload-lib/src/common/Utils.test.ts
+++ retroload-lib/src/common/Utils.test.ts
@@ -1,8 +1,18 @@
 import {BufferAccess} from './BufferAccess.js';
-import {calculateChecksum8} from './Utils.js';
+import {calculateChecksum8, hex16, hex8} from './Utils.js';
 
-test('calculateChecksum8', () => {
-  const ba = BufferAccess.create(16);
-  ba.writeAsciiString('EXAMPLE CONTENT');
-  expect(calculateChecksum8(ba)).toBe(71);
+describe('Utils', () => {
+  test('calculateChecksum8', () => {
+    const ba = BufferAccess.create(16);
+    ba.writeAsciiString('EXAMPLE CONTENT');
+    expect(calculateChecksum8(ba)).toBe(71);
+  });
+
+  test('hex8', () => {
+    expect(hex8(255)).toBe('0xff');
+  });
+
+  test('hex16', () => {
+    expect(hex16(4080)).toBe('0x0ff0');
+  });
 });
