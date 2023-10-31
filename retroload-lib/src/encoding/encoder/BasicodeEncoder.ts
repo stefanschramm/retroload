0ec1f84bc4225ca31d6dbf9bb62bf50b7dd12380
Stefan Schramm <mail@stefanschramm.net>
2023-10-31 17:01:04 +0100

    Add BASICODE adapter

---
+++ retroload-lib/src/encoding/encoder/BasicodeEncoder.ts
@@ -0,0 +1,88 @@
+import {type BufferAccess} from '../../common/BufferAccess.js';
+import {hex8} from '../../common/Utils.js';
+import {Logger} from '../../common/logging/Logger.js';
+import {shortpilotOption} from '../Options.js';
+import {AbstractEncoder} from './AbstractEncoder.js';
+
+const fZero = 1200;
+const fOne = 2400;
+
+const stx = 0x02;
+const sth = 0x01;
+const etx = 0x03;
+
+/**
+ * Encoder for LC 80
+ *
+ * Format description:
+ * - The Chip Shop - Basicode 2 - p. 8
+ * - http://www.kc85emu.de/scans/rfe0190/Basicode.htm
+ * - https://github.com/robhagemans/basicode/blob/master/BASICODE.rst
+ */
+export class BasicodeEncoder extends AbstractEncoder {
+  static override getTargetName() {
+    return 'basicode';
+  }
+
+  override begin() {
+    this.recordSilence(this.recorder.sampleRate / 2);
+  }
+
+  override end() {
+    this.recordSilence(this.recorder.sampleRate / 2);
+  }
+
+  public recordBasicProgram(ba: BufferAccess) {
+    this.record(ba, stx);
+  }
+
+  public recordBasicData(ba: BufferAccess) {
+    for (const chunkBa of ba.chunksPadded(1024, sth)) {
+      this.record(chunkBa, sth);
+    }
+  }
+
+  override recordByte(byte: number) {
+    this.recordBit(0);
+    super.recordByte(0x80 | byte);
+    this.recordBit(1);
+    this.recordBit(1);
+  }
+
+  recordBit(value: number) {
+    if (value) {
+      this.recordOscillations(fOne, 2);
+    } else {
+      this.recordOscillations(fZero, 1);
+    }
+  }
+
+  private record(ba: BufferAccess, startMarker: number) {
+    const shortpilot = this.options.isFlagSet(shortpilotOption);
+    this.recordOscillations(fOne, fOne * (shortpilot ? 3 : 5));
+
+    let checksum = 0;
+
+    checksum ^= startMarker;
+    this.recordByte(startMarker);
+
+    // ASCII bytes
+    for (let i = 0; i < ba.length(); i++) {
+      let asciiByte = ba.getUint8(i);
+      if (asciiByte === 0x0a) {
+        // replace '\n' with '\r' as required by BASICODE
+        asciiByte = 0x0d;
+      }
+      checksum ^= asciiByte;
+      this.recordByte(asciiByte);
+    }
+
+    checksum ^= etx;
+    this.recordByte(etx);
+
+    Logger.debug(`Checksum: ${hex8(checksum)}`);
+    this.recordByte(checksum);
+
+    this.recordOscillations(fOne, fOne); // 1 s
+  }
+}
