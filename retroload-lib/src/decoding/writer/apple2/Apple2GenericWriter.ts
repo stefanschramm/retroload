ca8dde87dd810a13465498275ad2a5afacdc4de3
Stefan Schramm <mail@stefanschramm.net>
2023-11-13 23:33:01 +0100

    Refactor Apple2 generic decoder

--- retroload-lib/src/decoding/writer/apple2/Apple2GenericWriter.ts
+++ retroload-lib/src/decoding/writer/apple2/Apple2GenericWriter.ts
@@ -1,7 +1,8 @@
 import {type SampleProvider} from '../../sample_provider/SampleProvider.js';
 import {StreamingSampleToHalfPeriodConverter} from '../../half_period_provider/StreamingSampleToHalfPeriodConverter.js';
 import {type WriterDefinition, type ConverterSettings, type OutputFile} from '../../ConverterManager.js';
-import {Apple2HalfPeriodProcessor, type FileDecodingResult, FileDecodingResultStatus} from './Apple2HalfPeriodProcessor.js';
+import {Apple2HalfPeriodProcessor} from './Apple2HalfPeriodProcessor.js';
+import {type FileDecodingResult, FileDecodingResultStatus} from '../FileDecodingResult.js';
 
 const definition: WriterDefinition = {
   to: 'apple2generic',
@@ -22,5 +23,11 @@ function * convert(sampleProvider: SampleProvider, settings: ConverterSettings):
 }
 
 function bufferAccessListToOutputFile(fdr: FileDecodingResult): OutputFile {
-  return {proposedName: undefined, data: fdr.data, proposedExtension: 'bin', begin: fdr.begin, end: fdr.end};
+  return {
+    proposedName: undefined,
+    data: fdr.blocks[0].data, // files don't consist of blocks
+    proposedExtension: 'bin',
+    begin: fdr.begin,
+    end: fdr.end,
+  };
 }
